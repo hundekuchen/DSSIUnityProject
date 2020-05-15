896daec3f89c3cd1fa6373c2a3aee39c988bb6fe
rhteller <rteller@dsseoul.org>
2020-05-15 15:36:06 +0900

    RT

---
+++ Assets/Scripts/Enemy/RaphaelEnemyBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaphaelEnemyBullet : MonoBehaviour
+{
+    float speed;
+    float destroytime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroytime = 2.0f;
+        speed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position += step * transform.forward;
+
+        //void OnTriggerEnter(Collider other)
+        //{
+        //   if (other.gameObject.CompareTag("player"))
+        //    {
+        //        other.gameObject.GetComponent<moveScript>().takeDamage(10);
+        //    }
+        //}
+    }
+}
