ae6648d84c8395bfb262e283f6422caddba57c38
probaschik <probaschik@gmx.de>
2020-05-15 15:18:06 +0900

    Paul

---
+++ Assets/Scripts/Enemy/PaulEnemyBulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaulEnemyBulletScript : MonoBehaviour
+{
+    float speed;
+    float age;
+    float destroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        destroyTime = 1.0f;
+        age = 0.0f;
+        speed = 30f;
+        //Debug.Log(transform.forward);
+        Destroy(gameObject, destroyTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position += step * transform.forward;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemy"))
+        {
+            Debug.Log("treffer");
+            other.gameObject.GetComponent<EnemyDamage>().takeDamage(10);
+        }
+    }
+}
